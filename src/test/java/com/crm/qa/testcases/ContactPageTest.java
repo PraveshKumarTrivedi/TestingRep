385bf72703e9c773f016e77b3785ca217dbc3ada
Anuj <sharmaanuj@hotmail.com>
2020-03-14 19:50:46 +0530

    added comment

--- src/test/java/com/crm/qa/testcases/ContactPageTest.java
+++ src/test/java/com/crm/qa/testcases/ContactPageTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
+/* new comment from anuj */
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.ContactsPage;
 import com.crm.qa.pages.HomePage;
